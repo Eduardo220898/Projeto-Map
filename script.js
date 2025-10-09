7f202425334c0072179d6016e5a7a0cbc2b73bb0
Eduardo220898 <eduardozollner@gmail.com>
2025-10-08 21:58:10 -0300

    forEach

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const list = document.querySelector('ul')
-const produtos = { name: 'X-Bacon', price: 27, vegan: false, src: './assets/xbacon.png' }
+const buttonShow = document.querySelector('.Show-All')
+let myLy = ''
 
 
-list.innerHTML = `
+function showAll() {
+    menuOptions.forEach((produtos) => {
+        myLy += `
         <li>
             <img src="${produtos.src}" alt="${produtos.name}">
             <p>${produtos.name}</p>
             <p class="preco">R$ ${produtos.price.toFixed(2)}</p>
         </li>
-`
+    `
+    })
+    list.innerHTML = myLy
+}
+
+buttonShow.addEventListener('click', showAll)
